57a58bd86bf3b3892f7922f00bedfe50120a2a7c
SiposVivien <161618821+Viviess@users.noreply.github.com>
2025-11-21 06:53:44 +0100

    képfelismerés

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 #IMAGE TO TEXT
 #AI for this: Salesforce/blip-image-captioning-large
+
+#pip install pillow
+#pip install transformers torch torchvision pillow
+#python.exe -m pip install --upgrade pip
+#pip install transformers pillow
+
 # Use a pipeline as a high-level helper
 from transformers import pipeline
 from PIL import Image
-import requests
-from transformers import AutoProcessor, BlipForConditionalGeneration
 from transformers import AutoProcessor, AutoModelForVision2Seq
-
-pipe = pipeline("image-to-text", model="Salesforce/blip-image-captioning-large")
-# Load model directly
-
 
 processor = AutoProcessor.from_pretrained("Salesforce/blip-image-captioning-large")
 model = AutoModelForVision2Seq.from_pretrained("Salesforce/blip-image-captioning-large")
 
-#példa
-#url = "http://images.cocodataset.org/val2017/000000039769.jpg"
-#image = Image.open(requests.get(url, stream=True).raw)
-#text = "A picture of"
-#inputs = processor(images=image, text=text, return_tensors="pt")
-#outputs = model(**inputs)
+image = Image.open("Images/noveny1.jpg")
+
+# Kulcs: ilyen prompttal kényszerítjük, hogy fajnevet próbáljon mondani
+prompt = "a photo of a plant species:"
+
+inputs = processor(images=image, text=prompt, return_tensors="pt")
+
+generated_ids = model.generate(**inputs, max_new_tokens=50)
+caption = processor.decode(generated_ids[0], skip_special_tokens=True)
+
+print("BLIP növényfelismerés:", caption)
